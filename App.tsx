2767b4c4ddb9cc06631814c59296fc504a8bf5d6
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-04-24 09:08:33 -0300

    chore: add typescript

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { Welcome } from './src/components/Welcome'
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Welcome title="React Native Bare Workflow com Typescript" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+interface Props {
+  title: string
+}
+
+export function Welcome({ title }: Props) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
