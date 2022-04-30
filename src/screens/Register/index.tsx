bf576cb2d0d30b10d86b55fb27478ef69d69db99
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-04-30 10:51:55 -0300

    feat: implement component forms

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionsTypes
+} from './styles';
+
+export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+      <Form>
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+          <TransactionsTypes >
+            <TransactionTypeButton
+              title="Income"
+              type="up"
+              onPress={() => handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              title="Outcome"
+              type="down"
+              onPress={() => handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
+        </Fields>
+        <Button title="Enviar" />
+      </Form>
+    </Container>
+  )
+}
